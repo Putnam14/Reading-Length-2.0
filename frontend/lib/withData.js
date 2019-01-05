fdb68dff8db10966d10a3385fc08b1725e356003
Putnam14 <putnambr76@gmail.com>
2019-01-05 15:38:51 -0700

    Preliminarily add support for connecting to Apollo

---
+++ frontend/lib/withData.js
@@ -0,0 +1,24 @@
+// Next-Apollo package to make sure server-side rendering works
+import withApollo from 'next-with-apollo'
+// You can import Apollo along with any 'link's you want, or do this
+import ApolloClient from 'apollo-boost'
+import { apolloEndpoint } from '../config'
+
+function createClient({ headers }) {
+  return new ApolloClient({
+    // Can change this if you have different Yoga APIs for dev, prod
+    uri: apolloEndpoint,
+    request: operation => {
+      operation.setContext({
+        fetchOptions: {
+          // Bring cookies along for the ride
+          credentials: 'include',
+        },
+        headers,
+      })
+    },
+  })
+}
+
+// Use to create client in _app.js
+export default withApollo(createClient)
