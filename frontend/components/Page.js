85aaa9ab22dbef3aba71fb123cd169b4ecf39ef1
Putnam14 <putnambr76@gmail.com>
2019-01-05 15:38:24 -0700

    Page styles component with global theme

---
+++ frontend/components/Page.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
+import Header from './Header'
+import Meta from './Meta'
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightGrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+theme.lightgrey = theme.lightGrey
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
+
+injectGlobal`
+html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`
+
+class Page extends Component {
+  render() {
+    const { children } = this.props
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    )
+  }
+}
+
+export default Page
