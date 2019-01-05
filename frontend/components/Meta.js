066dc79996d83d9aafe7b240cbd333ef89958ab3
Putnam14 <putnambr76@gmail.com>
2019-01-05 15:38:06 -0700

    Create a universal meta component

---
+++ frontend/components/Meta.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <title>Reading Length</title>
+  </Head>
+)
+
+export default Meta
