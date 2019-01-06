07c54f91386a78ccd82e32a7ee4ce8cbcbfec685
Putnam14 <putnambr76@gmail.com>
2019-01-05 21:47:38 -0700

    Style and add links

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -13,6 +13,57 @@ Router.onRouteChangeError = () => {
   NProgress.done()
 }
 
-const Header = () => <div>Header goes here</div>
+const StyledHeader = styled.div`
+  background-color: #f8f8f8;
+  border-bottom: 1px solid #e7e7e7;
+  .container {
+    margin: 0.5em auto;
+    width: 80vw;
+    max-width: 1170px;
+    display: grid;
+    grid-template-columns: 1fr auto;
+    align-items: center;
+  }
+  a {
+    text-decoration: none;
+    color: #777;
+    :hover {
+      color: #5e5e5e;
+    }
+  }
+`
+
+const Logo = styled.div`
+  font-size: 2rem;
+`
+
+const Nav = styled.nav`
+  a {
+    padding-right: 1em;
+  }
+`
+
+const Header = () => (
+  <StyledHeader>
+    <div className="container">
+      <Logo>
+        <Link href="/">
+          <a>Reading Length</a>
+        </Link>
+      </Logo>
+      <Nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/wpm">
+          <a>Tests</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </Nav>
+    </div>
+  </StyledHeader>
+)
 
 export default Header
