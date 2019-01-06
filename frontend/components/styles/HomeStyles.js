7371dd0b965cdf649ae79c100e81498542d11d48
Putnam14 <putnambr76@gmail.com>
2019-01-06 15:46:43 -0700

    Move styling out of Home component

---
+++ frontend/components/styles/HomeStyles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const HomeStyles = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  div {
+    display: grid;
+    justify-items: center;
+  }
+  h1 {
+    font-size: 4rem;
+  }
+  h2 {
+    font-size: 3rem;
+  }
+  h1,
+  h2 {
+    margin: 0;
+    font-weight: 500;
+  }
+`
+
+export default HomeStyles
