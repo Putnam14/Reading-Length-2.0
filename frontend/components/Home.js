d9f4eebb2b455aa28e2ae2187eb6b6df531bd962
Putnam14 <putnambr76@gmail.com>
2019-01-06 16:02:16 -0700

    Use Head to modify browser title

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import Search from './Search'
 import HomeStyles from './styles/HomeStyles'
 
 const Home = () => (
   <HomeStyles>
+    <Head>
+      <title>Book length search engine | Reading Length</title>
+    </Head>
     <div>
       <h1>Reading Length</h1>
       <h2>How long will it take to read that book?</h2>
