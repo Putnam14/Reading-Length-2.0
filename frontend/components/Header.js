ea0694b9d2e81e05cb531c007384748126ba925e
Putnam14 <putnambr76@gmail.com>
2019-01-06 15:46:31 -0700

    Move styling outside of header component

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,8 @@
 import Link from 'next/link'
-import styled from 'styled-components'
 import NProgress from 'nprogress'
 import Router from 'next/router'
+import Headroom from 'react-headroom'
+import HeaderStyles from './styles/HeaderStyles'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -13,57 +14,29 @@ Router.onRouteChangeError = () => {
   NProgress.done()
 }
 
-const StyledHeader = styled.div`
-  background-color: #f8f8f8;
-  border-bottom: 1px solid #e7e7e7;
-  .container {
-    margin: 0.5em auto;
-    width: 80vw;
-    max-width: 1170px;
-    display: grid;
-    grid-template-columns: 1fr auto;
-    align-items: center;
-  }
-  a {
-    text-decoration: none;
-    color: #777;
-    :hover {
-      color: #5e5e5e;
-    }
-  }
-`
-
-const Logo = styled.div`
-  font-size: 2rem;
-`
-
-const Nav = styled.nav`
-  a {
-    padding-right: 1em;
-  }
-`
-
 const Header = () => (
-  <StyledHeader>
-    <div className="container">
-      <Logo>
-        <Link href="/">
-          <a>Reading Length</a>
-        </Link>
-      </Logo>
-      <Nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/wpm">
-          <a>Tests</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </Nav>
-    </div>
-  </StyledHeader>
+  <Headroom>
+    <HeaderStyles>
+      <div className="container">
+        <div className="logo">
+          <Link href="/">
+            <a>Reading Length</a>
+          </Link>
+        </div>
+        <div rel="navigation">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/wpm">
+            <a>Tests</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </div>
+      </div>
+    </HeaderStyles>
+  </Headroom>
 )
 
 export default Header
---
+++ frontend/components/styles/HeaderStyles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+const HeaderStyles = styled.div`
+  background-color: #f8f8f8;
+  border-bottom: 1px solid #e7e7e7;
+  .container {
+    margin: 0 auto;
+    width: 80vw;
+    max-width: 1170px;
+    display: grid;
+    grid-template-columns: 1fr auto;
+    align-items: center;
+  }
+  a {
+    color: #777;
+    padding-right: 1em;
+    :hover {
+      color: #5e5e5e;
+    }
+  }
+  .logo {
+    font-size: 2rem;
+  }
+`
+
+export default HeaderStyles
