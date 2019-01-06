4eee64618023d5853dd0976213b5e31181778726
Putnam14 <putnambr76@gmail.com>
2019-01-05 18:37:04 -0700

    NProgress is cool

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,5 +1,17 @@
 import Link from 'next/link'
 import styled from 'styled-components'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
+Router.onRouteChangeError = () => {
+  NProgress.done()
+}
 
 const Header = () => <div>Header goes here</div>
 
