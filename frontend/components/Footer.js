c4aba04837d254385f99af66f35cd53dc083dc96
Putnam14 <putnambr76@gmail.com>
2019-01-06 15:46:14 -0700

    Add the footer

---
+++ frontend/components/Footer.js
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import FooterStyles from './styles/FooterStyles'
+
+const Footer = () => (
+  <FooterStyles>
+    <hr />
+    <div className="navLinks">
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/wpm">
+        <a>WPM Tests</a>
+      </Link>
+      <Link href="about">
+        <a>About</a>
+      </Link>
+    </div>
+    <p>
+      Made with ♥ in Boise by{' '}
+      <a href="https://bridgerputnam.me">Bridger Putnam</a>
+    </p>
+    <p>
+      Word count estimates are not guaranteed to be accurate. If you are an
+      author of a book or know of a book's accurate wordcount,{' '}
+      <a href="mailto:hey+RL@bridgerputnam.me">contact me</a>.
+    </p>
+    <p>
+      Reading Length is a participant in the Amazon Services LLC Associates
+      Program, an affiliate advertising program designed to provide a means for
+      us to earn fees by linking to Amazon.com and affiliated sites.
+    </p>
+  </FooterStyles>
+)
+
+export default Footer
---
+++ frontend/components/styles/FooterStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const FooterStyles = styled.footer`
+  p {
+    margin: 1rem 0
+    padding: 0;
+    font-size: 1.25rem;
+    color: #777;
+    line-height: 1.5rem;
+  }
+  .navLinks {
+    a {
+      padding-right: 1em;
+    }
+  }
+`
+
+export default FooterStyles
