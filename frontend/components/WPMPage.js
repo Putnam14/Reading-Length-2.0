45a95599bcdc1259f8f93e694295205685378eaf
Putnam14 <putnambr76@gmail.com>
2019-01-06 15:47:39 -0700

    WPM index component

---
+++ frontend/components/WPMPage.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import Search from './Search'
+
+const WPMPage = () => (
+  <div>
+    <Search />
+    <h1>WPM Speed Tests</h1>
+    <p>
+      Use one of our words per minute calculator to estimate your reading speed
+      as best as possible!
+    </p>
+    <p>
+      Find your reading speed in words per minute using any of the following
+      texts. Try to select the reading level that corresponds to the category of
+      book you will be reading. All text samples are sourced from classic books
+      provided by Project Gutenberg.
+    </p>
+    <h2>Easy or Elementary Reading Tests</h2>
+    <Link href="/wpm/ttopr/">
+      <a>The Tale of Peter Rabbit – Beatrix Potter</a>
+    </Link>
+    <h2>Moderate or High School Reading Tests</h2>
+    <Link href="/wpm/tpodg">
+      <a>The Picture of Dorian Gray – Oscar Wilde</a>
+    </Link>
+    <h2>Hard or Collegiate Reading Tests</h2>
+    <Link href="/wpm/atotc">
+      <a>A Tale of Two Cities – Charles Dickens</a>
+    </Link>
+  </div>
+)
+
+export default WPMPage
