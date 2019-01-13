d22919f1818320dfefe4203583f219c6e1512176
Putnam14 <putnambr76@gmail.com>
2019-01-13 16:47:12 -0700

    Set the prisma endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const apolloEndpoint = `http://localhost:4444`
+export const prismaEndpoint = `http://localhost:4444`
