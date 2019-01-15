2792d587aa1d01578309976765de42186d58d76b
Putnam14 <putnambr76@gmail.com>
2019-01-14 22:44:50 -0700

    bookPreviews -> bookPreview

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,7 +3,7 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   book: forwardTo("db"),
   wordCounts: forwardTo("db"),
-  bookPreviews: forwardTo("db")
+  bookPreview: forwardTo("db")
 };
 
 module.exports = Query;
