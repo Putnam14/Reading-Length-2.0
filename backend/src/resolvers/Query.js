7739394b95f5e6956faccb3e24f9a576197c17ee
Putnam14 <putnambr76@gmail.com>
2019-01-14 21:26:30 -0700

    Make wordCounts and relations queryable

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,9 @@
 const { forwardTo } = require("prisma-binding");
 
 const Query = {
-  book: forwardTo("db")
+  book: forwardTo("db"),
+  wordCounts: forwardTo("db"),
+  bookPreviews: forwardTo("db")
 };
 
 module.exports = Query;
