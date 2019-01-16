d2e5df224dc4dd20713cf619e9a9713ab7b07fca
Putnam14 <putnambr76@gmail.com>
2019-01-15 21:48:13 -0700

    Handle the book search

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,7 +3,22 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   book: forwardTo("db"),
   wordCounts: forwardTo("db"),
-  bookPreview: forwardTo("db")
+  bookPreview: forwardTo("db"),
+  bookSearch(parent, args, ctx, info) {
+    const { kw } = args;
+    // We'll want to split this out so we query amazon if we don't get a result
+    return ctx.db.query.bookIndexes(
+      {
+        where: {
+          OR: [
+            { name_contains: kw.toLowerCase() },
+            { author_contains: kw.toLowerCase() }
+          ]
+        }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
