f1e758c612fb73237637b68536f7159e69364e9e
Putnam14 <putnambr76@gmail.com>
2019-01-19 16:11:56 -0700

    Stub out method to search APIs

---
+++ backend/src/lib/searchAPI.js
@@ -0,0 +1,11 @@
+const searchAPI = searchTerm => {
+  // Query Amazon API for search term
+  // Parse results
+  // const book = ctx.db.mutation.createBook(...parsedData)
+  // return book.isbn
+  const isbn = "0553593714";
+  console.log("Searching");
+  return isbn;
+};
+
+module.exports = searchAPI;
