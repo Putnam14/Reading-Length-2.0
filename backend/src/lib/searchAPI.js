959b84cd85d4d07e292ff1a1f98f9c4defb2872f
Putnam14 <putnambr76@gmail.com>
2019-01-20 17:53:51 -0700

    Flesh out newBookSearch method

--- backend/src/lib/searchAPI.js
+++ backend/src/lib/searchAPI.js
@@ -1,11 +1,99 @@
-const searchAPI = searchTerm => {
-  // Query Amazon API for search term
-  // Parse results
-  // const book = ctx.db.mutation.createBook(...parsedData)
-  // return book.isbn
-  const isbn = "0553593714";
-  console.log("Searching");
-  return isbn;
-};
+const { bookSearch } = require("./api/amazon");
 
-module.exports = searchAPI;
+exports.newBookSearch = async (searchTerm, ctx, info) => {
+  // Query Amazon API for search term
+  const amazonSearch = await bookSearch(searchTerm);
+  // Parse results
+  const amazonResults = {
+    isbn10: amazonSearch.ASIN,
+    name: amazonSearch.ItemAttributes.Title,
+    author: amazonSearch.ItemAttributes.Author,
+    image: amazonSearch.LargeImage.URL,
+    description: amazonSearch.EditorialReviews.EditorialReview.Content,
+    publishDate: amazonSearch.ItemAttributes.PublicationDate,
+    pageCount: parseInt(amazonSearch.ItemAttributes.NumberOfPages),
+    related: [
+      amazonSearch.SimilarProducts.SimilarProduct[0].ASIN,
+      amazonSearch.SimilarProducts.SimilarProduct[1].ASIN,
+      amazonSearch.SimilarProducts.SimilarProduct[2].ASIN
+    ],
+    relatedDetails: [
+      {
+        name: amazonSearch.SimilarProducts.SimilarProduct[0].Title,
+        isbn10: amazonSearch.SimilarProducts.SimilarProduct[0].ASIN
+      },
+      {
+        name: amazonSearch.SimilarProducts.SimilarProduct[1].Title,
+        isbn10: amazonSearch.SimilarProducts.SimilarProduct[1].ASIN
+      },
+      {
+        name: amazonSearch.SimilarProducts.SimilarProduct[2].Title,
+        isbn10: amazonSearch.SimilarProducts.SimilarProduct[2].ASIN
+      }
+    ]
+  };
+  const {
+    isbn10,
+    name,
+    author,
+    image,
+    description,
+    publishDate,
+    pageCount,
+    related,
+    relatedDetails
+  } = amazonResults;
+
+  // Check if book already has a BookPreview
+  const bookPreview = await ctx.db.query.bookPreview({ where: { isbn10 } });
+  if (bookPreview) {
+    if (!bookPreview.image) {
+      ctx.db.mutation.updateBookPreview(
+        { data: { image }, where: { id: bookPreview.id } },
+        info
+      );
+    }
+  } else {
+    ctx.db.mutation.createBookPreview({ data: { isbn10, name, image } }, info);
+  }
+
+  // Add related books to bookPreview
+  relatedDetails.forEach(async detail => {
+    const detailPreview = await ctx.db.query.bookPreview({
+      where: { isbn10: detail.isbn10 }
+    });
+    if (!detailPreview) {
+      ctx.db.mutation.createBookPreview({ data: { ...detail } }, info);
+    }
+  });
+
+  // Take a look if book exists in BookIndex
+  const existsInIndex = await ctx.db.query.bookIndexes({ where: { isbn10 } });
+  if (!existsInIndex[0]) {
+    // Add to Book
+    const addedBook = await ctx.db.mutation.createBook(
+      {
+        data: {
+          isbn10,
+          name,
+          author,
+          image,
+          description,
+          publishDate,
+          pageCount,
+          related: { set: related }
+        }
+      },
+      info
+    );
+    console.log(addedBook);
+    // Add to BookIndex
+    if (addedBook) {
+      ctx.db.mutation.createBookIndex({ data: { isbn10, name, author } });
+    }
+  }
+
+  // Find the audiobook length
+  console.log(isbn10);
+  return isbn10;
+};
