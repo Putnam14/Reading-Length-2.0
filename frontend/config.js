4e7bb01f0e1fb3f740c5cbb5b832483bfd3d85a7
Putnam14 <putnambr76@gmail.com>
2019-01-26 14:36:52 -0700

    Set the correct staging endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const prodEndpoint = `http://localhost:4444`
-export const betaEndpoint = `http://localhost:4444`
+export const stagingEndpoint = `https://rlength-yoga-staging.herokuapp.com/`
 export const devEndpoint = `http://localhost:4444`
