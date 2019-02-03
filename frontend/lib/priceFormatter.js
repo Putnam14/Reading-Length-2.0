d0b92d10b705d4a4669d33b815bcbabc4755eaf9
Putnam14 <putnambr76@gmail.com>
2019-02-03 16:26:34 -0700

    Parse amount for formatMoney, use RegExp.exec() properly

--- frontend/lib/priceFormatter.js
+++ frontend/lib/priceFormatter.js
@@ -1,27 +1,30 @@
-const formatMoney = function(amount) {
+const formatMoney = amount => {
+  // Amount is stored and handled as cents
+  const parsedAmount = parseInt(amount)
   const options = {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
   }
-  // if its a whole, dollar amount, leave off the .00
-  if (amount % 100 === 0) options.minimumFractionDigits = 0
+  // Leave off the .00 for round numbers
+  if (parsedAmount % 100 === 0) options.minimumFractionDigits = 0
   const formatter = new Intl.NumberFormat('en-US', options)
-  return formatter.format(amount / 100)
+  return formatter.format(parsedAmount / 100)
 }
 
+// This could be moved to the backend as a 'Price' object type with money formatting on the front end.
 const priceFormatter = (prices, isbn) => {
   const formatted = []
   prices.forEach(price => {
     const marketPattern = new RegExp(/([^:]+)/)
-    const pricePattern = new RegExp(/([0-9]+)/g)
-    const marketplace = marketPattern.exec(price)
+    const pricePattern = new RegExp(/([0-9]+)/)
+    const marketplace = marketPattern.exec(price)[0]
     let affiliateLink = ''
     if (marketplace === 'Amazon')
       affiliateLink = `https://www.amazon.com/dp/${isbn}?tag=${
         process.env.AMAZON_AFF
       }`
-    const formattedPrice = formatMoney(pricePattern.exec(price))
+    const formattedPrice = formatMoney(pricePattern.exec(price)[0])
     formatted.push({
       affiliateLink,
       marketplace,
