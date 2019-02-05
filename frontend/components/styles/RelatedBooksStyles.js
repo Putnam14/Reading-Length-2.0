20ebe377610c6cfc7aed8fa3609aef3519508f9d
Putnam14 <putnambr76@gmail.com>
2019-02-04 21:18:21 -0700

    Use flex-box to style the related books container

---
+++ frontend/components/styles/RelatedBooksStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const RelatedBooksStyles = styled.div`
+  .related-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+  }
+`
+
+export default RelatedBooksStyles
