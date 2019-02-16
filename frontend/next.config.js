d9581022c99cd345ec33a39b90c0e0cbc607d062
Putnam14 <putnambr76@gmail.com>
2019-02-16 01:25:11 -0700

    Add the URL to an environment variable

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,13 +7,27 @@ const endpoints = {
   prod: 'https://localhost:2222',
 }
 
+const urls = {
+  dev: 'http://localhost:3000',
+  staging: 'https://staging.readinglength.com',
+  prod: 'https://www.readinglength.com',
+}
+
 const affiliates = {
   amazon: 'readleng-20',
 }
 
 let endpoint = endpoints.dev
-if (staging) endpoint = endpoints.staging
-if (prod) endpoint = endpoints.prod
+let url = urls.dev
+
+if (staging) {
+  endpoint = endpoints.staging
+  url = urls.staging
+}
+if (prod) {
+  endpoint = endpoints.prod
+  url = urls.prod
+}
 
 module.exports = {
   target: 'serverless',
@@ -21,5 +35,6 @@ module.exports = {
     ENDPOINT: endpoint,
     SERVER: process.env.SERVER,
     AMAZON_AFF: affiliates.amazon,
+    URL: url,
   },
 }
