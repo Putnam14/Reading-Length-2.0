41e4943278a9f043394df11858736e57d3f44c6d
Putnam14 <putnambr76@gmail.com>
2019-02-16 01:24:55 -0700

    Meta tags

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -8,11 +8,38 @@ const Meta = () => (
     <title>Reading Length</title>
     <meta
       name="description"
+      key="description"
+      content="Find out how long it will take you to read any book by using our book word count search. Search for a book, tell us how fast you read, and start reading!"
+    />
+    <meta
+      property="og:description"
+      key="og-description"
+      content="Find out how long it will take you to read any book by using our book word count search. Search for a book, tell us how fast you read, and start reading!"
+    />
+    <meta
+      name="twitter:description"
+      key="twitter-description"
       content="Find out how long it will take you to read any book by using our book word count search. Search for a book, tell us how fast you read, and start reading!"
     />
     <meta
       property="og:title"
+      key="og-title"
       content="Reading Length | Search engine for books"
+    />
+    <meta
+      name="twitter:title"
+      key="twitter-title"
+      content="Reading Length | Search engine for books"
+    />
+    <meta
+      property="og:image"
+      key="og-image"
+      content={`${process.env.URL}/static/img/Logo_black.png`}
+    />
+    <meta
+      name="twitter:image"
+      key="twitter-image"
+      content={`${process.env.URL}/static/img/Logo_black.png`}
     />
     <link
       rel="shortcut icon"
