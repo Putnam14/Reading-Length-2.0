152e74fd172abc5a2c618aa332eec9f90380e037
Putnam14 <putnambr76@gmail.com>
2019-02-15 23:32:33 -0700

    Add favicon and logo

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -6,6 +6,20 @@ const Meta = () => (
     <meta charSet="utf-8" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <title>Reading Length</title>
+    <meta
+      name="description"
+      content="Find out how long it will take you to read any book by using our book word count search. Search for a book, tell us how fast you read, and start reading!"
+    />
+    <meta
+      property="og:title"
+      content="Reading Length | Search engine for books"
+    />
+    <link
+      rel="shortcut icon"
+      href="/static/img/favicon.ico"
+      type="image/x-icon"
+    />
+    <link rel="icon" href="/static/img/Logo_black.png" type="image/x-icon" />
   </Head>
 )
 
