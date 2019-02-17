f8030dffea70ac07ece0b8097e7a3c3cdc4105a9
Putnam14 <putnambr76@gmail.com>
2019-02-16 17:11:33 -0700

    Check user-agent to see if it's a bot/web crawler

--- frontend/lib/withApollo.js
+++ frontend/lib/withApollo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import { getDataFromTree } from 'react-apollo'
+import isBot from 'isbot'
 import initApollo from './init-apollo'
 
 export default App =>
@@ -9,37 +10,40 @@ export default App =>
 
     static async getInitialProps(ctx) {
       const { Component, router } = ctx
+      const userAgent = ctx.ctx.req.headers['user-agent']
+      const bot = isBot(userAgent)
 
       let appProps = {}
       if (App.getInitialProps) {
         appProps = await App.getInitialProps(ctx)
+        appProps.bot = bot
       }
 
       // Run all GraphQL queries in the component tree
       // and extract the resulting data
       const apollo = initApollo()
-      // if (!process.browser) {
-      try {
-        // Run all GraphQL queries
-        await getDataFromTree(
-          <App
-            {...appProps}
-            Component={Component}
-            router={router}
-            apolloClient={apollo}
-          />
-        )
-      } catch (error) {
-        // Prevent Apollo Client GraphQL errors from crashing SSR.
-        // Handle them in components via the data.error prop:
-        // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
-        console.error('Error while running `getDataFromTree`', error)
-      }
+      if (!bot) {
+        try {
+          // Run all GraphQL queries
+          await getDataFromTree(
+            <App
+              {...appProps}
+              Component={Component}
+              router={router}
+              apolloClient={apollo}
+            />
+          )
+        } catch (error) {
+          // Prevent Apollo Client GraphQL errors from crashing SSR.
+          // Handle them in components via the data.error prop:
+          // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
+          console.error('Error while running `getDataFromTree`', error)
+        }
 
-      // getDataFromTree does not call componentWillUnmount
-      // head side effect therefore need to be cleared manually
-      Head.rewind()
-      // }
+        // getDataFromTree does not call componentWillUnmount
+        // head side effect therefore need to be cleared manually
+        Head.rewind()
+      }
 
       // Extract query data from the Apollo store
       const apolloState = apollo.cache.extract()
